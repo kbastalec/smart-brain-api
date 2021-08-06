fe1cf5c3ddc337e9bcfd23aae159b968705de717
kbastalec <kristian.bastalec@gmail.com>
2021-08-06 14:32:14 +0200

    fixing port

--- server.js
+++ server.js
@@ -12,9 +12,9 @@ process.env.NODE_TLS_REJECT_UNAUTHORIZED = 0;
 
 const db = knex({
   client: 'pg',
-  connection: {
-    connectionString : process.env.DATABASE_URL,
-    ssl: true;
+  connectionString: process.env.DATABASE_URL,
+  ssl: {
+    rejectUnauthorized: false
   }
 });
 
