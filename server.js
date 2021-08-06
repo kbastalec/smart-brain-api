28ba08ddf8f1b02d4298fe675fdf25a50845b57c
kbastalec <kristian.bastalec@gmail.com>
2021-08-06 14:39:35 +0200

    fixing port

--- server.js
+++ server.js
@@ -8,13 +8,13 @@ const signin = require('./controllers/signin');
 const profile = require('./controllers/profile');
 const image = require('./controllers/image');
 
-process.env.NODE_TLS_REJECT_UNAUTHORIZED = 0;
+
 
 const db = knex({
   client: 'pg',
-  connectionString: process.env.DATABASE_URL,
-  ssl: {
-    rejectUnauthorized: false
+  connection: {
+    connectionString: process.env.DATABASE_URL,
+    ssl: true,
   }
 });
 
